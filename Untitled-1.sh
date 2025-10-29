458e04fdbff30de2bbf3b15e1039e4455698f2c8
Raiza Tocolo <tpsaira1@gmail.com>
2025-10-29 19:20:08 +0100

    solo vpc funcional

--- Untitled-1.sh
+++ Untitled-1.sh
@@ -1,4 +1,12 @@
-aws ec2 create-vpc --cidr-block 10.0.0.0/24 --query Vpc.VpcId --output text \
+VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.0.0/24 --query Vpc.VpcId --output text \
     --tag-specifications ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]\
-    --query Vpc.VpcId  --output text 
+    --query Vpc.VpcId  --output text)
 
+
+#muestro la vpc
+echo $VPC_ID
+
+#habilitar dns en la vpc
+aws ec2 modify--vpc-atribute \
+    --vpc-id $VPC_ID \
+    --enable-dns-hostnames "{\value\":tue}"
---
+++ scrip1.sh
@@ -0,0 +1,12 @@
+VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.0.0/24 --query Vpc.VpcId --output text \
+    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
+    --query Vpc.VpcId  --output text)
+
+
+#muestro la vpc
+echo $VPC_ID
+
+#habilitar dns en la vpc
+aws ec2 modify-vpc-attribute \
+    --vpc-id $VPC_ID \
+    --enable-dns-hostnames "{\"Value\":true}"
